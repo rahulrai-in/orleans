c7d466f5650461a6460c773c2aa631eeb311aed4
moonytheloony <rahulrai@live.com>
2015-09-05 21:03:29 +0530

    base projects

---
+++ OrleansInterfaces/IGrain1.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Orleans;
+
+namespace OrleansInterfaces
+{
+    /// <summary>
+    /// Grain interface IGrain1
+    /// </summary>
+    public interface IGrain1 : IGrain
+    {
+
+    }
+}
---
+++ OrleansWebApplicaton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrleansWebApplicaton.Startup))]
+namespace OrleansWebApplicaton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
